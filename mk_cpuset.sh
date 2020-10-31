b8dd34a928160896cdd6f2d05f08721cfcc8b7e8
Andre Merzky <andre@merzky.net>
2020-11-01 00:02:32 +0100

    sudo

--- mk_cpuset.sh
+++ mk_cpuset.sh
@@ -1,19 +1,19 @@
 
 BASE=/dev/cpuset
-UID=$1  ; shift
+NAME=$1  ; shift
 CORES=$1; shift
 CMD=$*
 
-test -z "$CMD"     && echo "no cmd"
-test -z "$CMD"     && exit 1
+test -z "$CMD"      && echo "no cmd"
+test -z "$CMD"      && exit 1
 
-test -d $BASE      || echo "cpuset not available"
-test -d $BASE      || exit 1
+test -d $BASE       || echo "cpuset not available"
+test -d $BASE       || exit 1
 
-test -d $BASE/$UID || mkdir $BASE/$UID
+test -d $BASE/$NAME || mkdir $BASE/$NAME
 
-echo "$CORES" >> $BASE/$UID/cpuset.cpus
-echo "$CORES" >> $BASE/$UID/cpuset.mems
-echo "$$"     >> $BASE/$UID/tasks
+echo "$CORES" | sudo tee -a $BASE/$NAME/cpuset.cpus
+echo "$CORES" | sudo tee -a $BASE/$NAME/cpuset.mems
+echo "$$"     | sudo tee -a $BASE/$NAME/tasks
 exec $cmd
 
